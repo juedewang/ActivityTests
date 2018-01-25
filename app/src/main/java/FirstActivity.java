a839c89ecfa0d9f02a45c36a90346a609caa498a
Jue Wang <juedewang@gmail.com>
2018-01-25 21:19:02 +1100

    First commit

---
+++ app/src/main/java/FirstActivity.java
@@ -0,0 +1,14 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jue on 25/1/18.
+ */
+
+public class FirstActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
+
